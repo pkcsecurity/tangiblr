817c884daab4072c2558b7424bd7b52e07a64e33
Becker Polverini <becker@pkcsecurity.com>
2019-06-27 13:20:53 -0700

    alex :knife:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Slider, {Range} from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
-function App() {
+const Nav = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nav">
+      <div className="nav__name">Tangiblr</div>
     </div>
   );
-}
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+const Body = () => {
+  const [customers, setCustomers] = useState(0);
+
+  return (
+    <div className="body">
+      <div className="hero">
+        <div className="hero__lead">We send mail that makes money.</div>
+        <div className="hero__description">
+          Tangiblr integrates with your Shopify store so you convert more
+          customers for more sales.
+        </div>
+        <a href="#">
+          <div className="hero__c2a">Get Started</div>
+        </a>
+      </div>
+      <div className="sliders">
+        <h1>How much can I earn with Tangiblr?</h1>
+        <h2>Customers per Month</h2>
+        <div className="sliders__container">
+          <Slider
+            min={1}
+            max={100000}
+            className="slider"
+            onChange={x => setCustomers(x)}
+          />
+          <h3>{customers}</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <Nav />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
