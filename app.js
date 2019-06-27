6d84b70de6ded6c1d9989bd07b574fd6bbf859e3
Justin Chang <justinjunochang@gmail.com>
2019-06-27 13:28:22 -0700

    add shopify

--- app.js
+++ app.js
@@ -6,16 +6,23 @@ const serve = require('koa-static');
 const mount = require('koa-mount');
 const cors = require('koa-cors');
 const HttpStatus = require('http-status');
+const Shopify = require('shopify-api-node');
 
 const app = new Koa();
-
-const PORT = process.env.PORT || 3000;
-
 app.use(serve('frontend/build'));
-
 app.use(BodyParser());
 app.use(Logger());
-app.use(cors());
+// app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+
+const shopify = new Shopify({
+  shopName: 'tangiblr.myshopify.com',
+  apiKey: apiKey,
+  password: apiSecret,
+});
 
 const router = new Router();
 
@@ -26,8 +33,15 @@ router.get('/book', async (ctx, next) => {
   await next();
 });
 
+router.get('/users', async (ctx, next) => {
+  const users = await shopify.user.list();
+  console.log(users);
+  await next();
+})
+
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, function() {
   console.log('==> 🌎 Listening on port %s. Visit http://localhost:%s/', PORT, PORT);
+  
 });
