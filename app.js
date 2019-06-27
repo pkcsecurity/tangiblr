fd5a7d052ae165545e15d7da3570ee7da4ef438f
alex joo <alex@pkcsecurity.com>
2019-06-27 12:32:49 -0700

    removes unnecessary folders, adds static mount

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const Koa = require("koa");
-const BodyParser = require("koa-bodyparser");
-const Router = require("koa-router");
-const Logger = require("koa-logger");
-const serve = require("koa-static");
-const mount = require("koa-mount");
+const Koa = require('koa');
+const BodyParser = require('koa-bodyparser');
+const Router = require('koa-router');
+const Logger = require('koa-logger');
+const serve = require('koa-static');
+const mount = require('koa-mount');
 const cors = require('koa-cors');
-const HttpStatus = require("http-status");
+const HttpStatus = require('http-status');
 
 const app = new Koa();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(serve('frontend/build'));
 
 app.use(BodyParser());
 app.use(Logger());
@@ -17,61 +19,24 @@ app.use(cors());
 
 const router = new Router();
 
-router.get("/book", async (ctx, next) => {
-    const books = ["Speaking javascript", "Fluent Python", "Pro Python", "The Go programming language"];
-    ctx.status = HttpStatus.OK;
-    ctx.body = books;
-    await next();
+router.get('/book', async (ctx, next) => {
+  const books = [
+    'Speaking javascript',
+    'Fluent Python',
+    'Pro Python',
+    'The Go programming language',
+  ];
+  ctx.status = HttpStatus.OK;
+  ctx.body = books;
+  await next();
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
-
-app.listen(PORT, function () {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+app.listen(PORT, function() {
+  console.log(
+    '==> 🌎  Listening on port %s. Visit http://localhost:%s/',
+    PORT,
+    PORT,
+  );
 });
-
-
-
-
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-//
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-//
-// var app = express();
-//
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-//
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-//
-// module.exports = app;
