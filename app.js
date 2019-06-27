fa15341bf2990c6cd0c231ad264622a8abe5c146
Justin Chang <justinjunochang@gmail.com>
2019-06-27 16:12:22 -0700

    integrate shopify

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const serve = require('koa-static');
 const mount = require('koa-mount');
 const cors = require('koa-cors');
 const HttpStatus = require('http-status');
-const Shopify = require('shopify-api-node');
+const respond = require('koa-respond');
 
 // Koa middleware
 const shopifyRoutes = require('./middleware/shopify');
@@ -15,29 +15,18 @@ const app = new Koa();
 app.use(serve('frontend/build'));
 app.use(BodyParser());
 app.use(Logger());
+app.use(respond());
 // app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 
-const shopify = new Shopify({
-  shopName: 'tangiblr.myshopify.com',
-  apiKey: apiKey,
-  password: apiSecret,
-});
-
 const router = new Router();
 
-router.get('/book', async (ctx, next) => {
-  const books = ['Speaking javascript', 'Fluent Python', 'Pro Python', 'The Go programming language'];
-  ctx.status = HttpStatus.OK;
-  ctx.body = books;
-  await next();
-});
-
-router.get('/customers', shopifyRoutes.getCustomers);
-
+router.post('/customers', shopifyRoutes.getCustomersByUsername);
+router.post('/signup', shopifyRoutes.addUser);
+router.post('/login', shopifyRoutes.login);
 
 app.use(router.routes()).use(router.allowedMethods());
 
--- middleware/shopify.js
+++ middleware/shopify.js
@@ -1,3 +1,49 @@
+const Shopify = require('shopify-api-node');
+
+const users = {};
+
+const addUser = async (ctx, next) => {
+  const { username, password, shopName, apiKey, apiSecret } = ctx.request.body;
+
+  if (!username || !password || !shopName || !apiKey || !apiSecret) 
+    ctx.send(400, "Blank field");
+
+  const newUser = {
+    password,
+    shopName,
+    apiKey,
+    apiSecret
+  };
+  users[username] = newUser;
+
+  ctx.send(204);
+}
+
+const login = async (ctx, next) => {
+  const { username, password } = ctx.request.body;
+
+  if (!username || !password)
+    ctx.send(400, "Blank field");
+
+  if (password == users[username].password) 
+    ctx.send(204);
+  else 
+    ctx.send(401);
+}
+
+const getCustomersByUsername = async (ctx, next) => {
+  const username = ctx.request.body.username;
+  const { shopName, apiKey, apiSecret } = users[username];
+
+  const shopify = new Shopify({
+    shopName,
+    apiKey,
+    password: apiSecret
+  });
+
+  ctx.send(200, shopify.customer.list());
+}
+
 const getCustomers = async (ctx, next) => {
   const users = await shopify.customer.list();
   console.log(users);
@@ -5,5 +51,8 @@ const getCustomers = async (ctx, next) => {
 }
 
 module.exports = {
-  getCustomers
+  users,
+  addUser,
+  login,
+  getCustomersByUsername
 };
