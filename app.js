d3a4092f4b258eb867b5bc11d8966e905751aac0
alex joo <alex@pkcsecurity.com>
2019-06-27 13:15:52 -0700

    fixed gitignores and ports

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HttpStatus = require('http-status');
 
 const app = new Koa();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(serve('frontend/build'));
 
